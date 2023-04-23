8d096335b4af020db0da4f5e33c7c53a260f1ee2
Rodrigo Augusto Silva Lima <rodrigoaslima@hotmail.com>
2023-04-23 12:44:17 -0300

    finalizado tela de dados

--- PROJETO API/Site/app.py
+++ PROJETO API/Site/app.py
@@ -19,10 +19,35 @@ def index():
 
 @app.route("/dados.html", methods=['GET', 'POST'])
 def dados():
+    data=[
+        ("JAN", 22,30),
+        ("FEV", 25,20),
+        ("MAR", 12, 42),
+        ("ABR", 20,32),
+        ("MAI", 78,60),
+        ("JUN", 47,43),
+        ("JUL", 50,71),
+        ("AGO", 90,87),
+        ("SET", 74,59),
+        ("OUT", 78,71),
+        ("NOV", 80,95),
+        ("DEZ", 100,100),
+    ]
+
+    labels = [row[0] for row in data]
+    values1 = [row[1] for row in data]
+    values2 = [row[2] for row in data]
+
+
+    Consultas=['Pneumologista','Cardiologista','Clinico Geral']
+    Cidades= ['Jacareí','São José dos Campos','Taubaté']
+    Anos=['2019','2020','2021','2022']
+    
+
     global botao
     if request.method == 'POST':
         botao = request.form.get('botao')
-    return render_template("/dados/dados.html", resultado=botao)
+    return render_template("/dados/dados.html", resultado=botao, labels=labels, values1=values1, values2=values2, Consultas=Consultas, Cidades=Cidades, Anos=Anos)
 
 # @app.route("/conheca.html")
 # def quem_somos():
